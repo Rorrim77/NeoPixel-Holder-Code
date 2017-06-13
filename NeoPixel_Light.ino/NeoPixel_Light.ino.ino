38f6dcd67d26c840284ef09490167d17461c94b4
Sebastiaan Holman <Sebastiaan Holman>
2017-06-13 11:17:25 +0200

    Code added

---
+++ NeoPixel_Light.ino/NeoPixel_Light.ino.ino
@@ -0,0 +1,112 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN            6
+
+// Which pin on the arduino is connected to the button?
+#define buttonPin      3
+
+// Which pin on the arduino is the status led connected to? (default on board: 13)
+#define ledPin         13
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      8
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 50; // delay in ms
+int lightMode = 1;
+
+
+// Variables will change:
+int ledState = HIGH;         // the current state of the output pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library. 
+  
+  pinMode(buttonPin, INPUT); // set the button pin to input
+  pinMode(ledPin, OUTPUT); // set the ledpin to output
+
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+  // turn on the neopixel bar leds if ledstate is high
+  if (ledState == HIGH){
+    on();
+  } else {
+    off();
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+
+  // some delay added to give the controller some time to rest
+  delay(delayval);
+}
+
+void off() {
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  for (int i = 0; i < NUMPIXELS; i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+}
+
+void on() {
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  for (int i = 0; i < NUMPIXELS; i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(255, 255, 255)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+}
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# NeoPixel-Holder-Code" 
+
+Please install the Adafruit_NeoPixel library in the Arduino IDE before compiling and uploading this code.
+
+This code is used in conjunction with https://www.thingiverse.com/thing:2381667
